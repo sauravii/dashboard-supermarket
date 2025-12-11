8aa14deab012ffcd631ac433f268627ef58ed6a6
Sheren Aura Vi <sherenaura21@gmail.com>
2025-12-11 10:10:16 +0700

    Revert "create activity log feature"
    This reverts commit a1ef2723dc7d0cd6d3f6b3175fd8821855f43029.

--- controllers/admin/role_controller.php
+++ controllers/admin/role_controller.php
@@ -1,18 +1,9 @@
 <?php
 require_once(__DIR__ . '/../../models/role_model.php');
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
 // CREATE
 function create_role_controller($role_name) {
-    $result = insert_role($role_name);
-    if ($result) {
-        $current_user_id = $_SESSION['user_id'];
-        create_log_controller($current_user_id, "Add Role", "Menambahkan role $role_name");
-    }
-    return $result;
+  return insert_role($role_name);
 }
 
 // READ
@@ -27,22 +18,11 @@ function read_role_by_id_controller($role_id) {
 
 // UPDATE
 function update_role_controller($role_id, $role_name) {
-    $result = update_role($role_id, $role_name);
-    if ($result) {
-        $current_user_id = $_SESSION['user_id'];
-        create_log_controller($current_user_id, "Edit Role", "Mengubah role ID $role_id menjadi $role_name");
-    }
-    return $result;
+  return update_role($role_id, $role_name);
 }
 
 // DELETE
 function delete_role_controller($role_id) {
-    $result = delete_role($role_id);
-    if ($result) {
-        $current_user_id = $_SESSION['user_id'];
-        create_log_controller($current_user_id, "Delete Role", "Menghapus role ID $role_id");
-    }
-    return $result;
+  return delete_role($role_id);
 }
-
 
